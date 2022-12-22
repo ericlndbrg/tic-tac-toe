b1ac67a33fba17578e73d98236daf10415921851
ericlndbrg <ericrlindberg@gmail.com>
2022-12-22 10:29:58 -0500

    rubocop'd tic-tac-toe.rb

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require_relative 'classes/game'
 require 'byebug'
 
 def main
-	game = Game.new
-	game.play
+  game = Game.new
+  game.play
 end
 
 main
