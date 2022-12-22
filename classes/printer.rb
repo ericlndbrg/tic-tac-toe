87aee3bfb126d07b336b1ad3e8146cc9ffb0fb8f
ericlndbrg <ericrlindberg@gmail.com>
2022-12-22 12:25:41 -0500

    rubocop'd classes/printer.rb

--- classes/printer.rb
+++ classes/printer.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# this class prints output to the console
 class Printer
-	def self.print_output(msg, add_newline_char: true)
-		add_newline_char ? puts(msg) : print(msg)
-	end
+  def self.print_output(msg, add_newline_char: true)
+    add_newline_char ? puts(msg) : print(msg)
+  end
 end
