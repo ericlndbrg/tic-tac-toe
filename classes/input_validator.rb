c8d205c8c93f99a8a930495c74d53ee47a78c32c
ericlndbrg <ericrlindberg@gmail.com>
2022-12-22 11:41:27 -0500

    rubocop'd classes/input_validator.rb

--- classes/input_validator.rb
+++ classes/input_validator.rb
@@ -1,18 +1,23 @@
+# frozen_string_literal: true
+
+# this class validates the user-submitted input
 class InputValidator
-	VALID_USER_INPUT = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3', 'q'].freeze
+  VALID_USER_INPUT = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3 q].freeze
 
-	def validate_user_input(selection, cell_value)
-		# check if the input is allowed
-		unless VALID_USER_INPUT.include?(selection)
-			raise(StandardError, 'invalid input, please try again')
-		end
+  def validate_user_input(selection, cell_value)
+    raise_invalid_input_exception unless VALID_USER_INPUT.include?(selection)
+    return if selection == 'q'
 
-		# break out if selection == 'q'
-		return if selection == 'q'
+    raise_cell_already_taken_exception unless cell_value == ' '
+  end
 
-		# check if the selected cell is available
-		unless cell_value == ' '
-			raise(StandardError, 'that cell is already taken, please try again')
-		end
-	end
+  private
+
+  def raise_invalid_input_exception
+    raise(StandardError, 'invalid input, please try again')
+  end
+
+  def raise_cell_already_taken_exception
+    raise(StandardError, 'that cell is already taken, please try again')
+  end
 end
