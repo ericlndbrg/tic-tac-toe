4c26835d1f5a7266ab32e912b8b12a5853a999ef
ericlndbrg <ericrlindberg@gmail.com>
2022-12-22 11:08:39 -0500

    mostly rubocop'd classes/grid.rb

--- classes/grid.rb
+++ classes/grid.rb
@@ -1,34 +1,37 @@
+# frozen_string_literal: true
+
+# this class manages and draws the grid
 class Grid
-	attr_reader :cell_hash
+  attr_reader :cell_hash
 
-	def initialize
-		self.cell_hash = {
-		  'a1' => ' ', 'a2' => ' ', 'a3' => ' ',
-		  'b1' => ' ', 'b2' => ' ', 'b3' => ' ',
-		  'c1' => ' ', 'c2' => ' ', 'c3' => ' '
-		}
-	end
+  def initialize
+    self.cell_hash = {
+      'a1' => ' ', 'a2' => ' ', 'a3' => ' ',
+      'b1' => ' ', 'b2' => ' ', 'b3' => ' ',
+      'c1' => ' ', 'c2' => ' ', 'c3' => ' '
+    }
+  end
 
-	def draw
-		[
-			'    1   2   3',
-			'  +-----------+',
-			"A | #{self.cell_hash['a1']} | #{self.cell_hash['a2']} | #{self.cell_hash['a3']} |",
-			'  -------------',
-			"B | #{self.cell_hash['b1']} | #{self.cell_hash['b2']} | #{self.cell_hash['b3']} |",
-			'  -------------',
-			"C | #{self.cell_hash['c1']} | #{self.cell_hash['c2']} | #{self.cell_hash['c3']} |",
-			'  +-----------+'
-		].each do |grid_row|
-			Printer.print_output(grid_row)
-		end
-	end
+  def draw
+    [
+      '    1   2   3',
+      '  +-----------+',
+      "A | #{cell_hash['a1']} | #{cell_hash['a2']} | #{cell_hash['a3']} |",
+      '  -------------',
+      "B | #{cell_hash['b1']} | #{cell_hash['b2']} | #{cell_hash['b3']} |",
+      '  -------------',
+      "C | #{cell_hash['c1']} | #{cell_hash['c2']} | #{cell_hash['c3']} |",
+      '  +-----------+'
+    ].each do |grid_row|
+      Printer.print_output(grid_row)
+    end
+  end
 
-	def update(selection, char)
-		self.cell_hash[selection] = char
-	end
+  def update(selection, char)
+    self.cell_hash[selection] = char
+  end
 
-	private
+  private
 
-	attr_writer :cell_hash
+  attr_writer :cell_hash
 end
